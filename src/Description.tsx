ad4e76ba1e1beafb7f0f388933d8aad0e2dc203a
fandytcc <fandytsui@gmail.com>
2018-03-23 11:10:50 +0100

    create class component Description with Props & State interface

---
+++ src/Description.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+
+interface Props {
+  countBy?: number
+}
+
+interface State {
+  count: number
+}
+
+class Description extends React.Component<Props, State> {
+  public static defaultProps: Partial<Props> = {
+    countBy: 1,
+  }
+
+  state: State = {
+    count: 0,
+  }
+
+//! indicates that we know the value will exist and to not throw a potential undefined error
+  increase = () => {
+    const countBy: number = this.props.countBy!
+    const count = this.state.count + countBy
+    this.setState({ count })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          My favourite number is { this.state.count }
+        </p>
+        <button onClick={this.increase}>
+          Increase
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Description
