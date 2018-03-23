dfa4b89d67594c23110e26a568f496ac195abb62
fandytcc <fandytsui@gmail.com>
2018-03-23 10:39:26 +0100

    create stateless functional component Header

---
+++ src/Header.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+//define a props interface for each component, no longer need prop-types package
+//must declare an interface for the state of class components
+interface Props {
+  name?: string
+}
+//? operator = to mark defaultProps as optional; optional props are union of type undefined | string
+
+//type SFC<P> (Stateless Functional Component - allows us to use defaultProps)= alias of interface StatelessComponent<P>, has pre-defined children, defaultProps, displayName, etc. Don't need to define children?: ReactNode every time on our own.
+const Header: React.SFC<Props> = (props: Props) => (
+  <h1>
+    Hello, {props.name}! Welcome to React and TypeScript.
+  </h1>
+)
+
+Header.defaultProps = {
+  name: 'world'
+}
+
+export default Header
